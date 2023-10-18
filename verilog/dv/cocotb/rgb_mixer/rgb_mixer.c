38d80501d79fe4605237cef61a55824923191f59
zerotoasic <zerotoasic@zerotoasic.vm.com>
2023-10-18 12:18:04 +0200

    reduce clutter in tb c file

--- verilog/dv/cocotb/rgb_mixer/rgb_mixer.c
+++ verilog/dv/cocotb/rgb_mixer/rgb_mixer.c
@@ -1,11 +1,9 @@
 #include <firmware_apis.h> // include required APIs
 void main(){
-   // Enable managment gpio as output to use as indicator for finishing configuration 
-   flash_phy_clk_divisor_write(0);
+   flash_phy_clk_divisor_write(0); // slightly speed things up
    ManagmentGpio_outputEnable();
    ManagmentGpio_write(0);
-   enableHkSpi(0); // disable housekeeping spi
-   //GPIOs_configureAll(GPIO_MODE_MGMT_STD_OUT);
+
    for (int pin = 8 ; pin <= 13; pin ++)
 	   GPIOs_configure(pin, GPIO_MODE_USER_STD_INPUT_NOPULL);
    for (int pin = 14 ; pin <= 17; pin ++)
@@ -15,8 +13,7 @@ void main(){
    LogicAnalyzer_outputEnable(LA_REG_0, 0);
    LogicAnalyzer_write(LA_REG_0, 1);
    LogicAnalyzer_write(LA_REG_0, 0);
-   ManagmentGpio_write(1); // configuration finished 
-
+   ManagmentGpio_write(1);
    
    return;
 }
